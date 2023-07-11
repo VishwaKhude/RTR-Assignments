d3e0804be649429e930083f85e335b816e001eeb
VishwaKhude <97826054+VishwaKhude@users.noreply.github.com>
2023-07-11 08:24:18 +0530

    Pointers - 11 and 12

---
+++ 03_C_Assignments/14-Pointers/03-Arrays/04-Addresses/04-CharArray/01-WithoutPointers/CharArrayAddresses.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //variable declarations
+    char cArray[10];
+    int i;
+
+    //code
+    for (i = 0; i < 10; i++)
+        cArray[i] = (char)(i + 65);
+
+    printf("\n\n");
+    printf("Elements Of The Charactere Array : \n\n");
+    for (i = 0; i < 10; i++)
+        printf("cAray[%d] = %c\n", i, cArray[i]);
+
+    printf("\n\n");
+    printf("Elements Of The Character Array : \n\n");
+    for (i = 0; i < 10; i++)
+        printf("cArray[%d] = %c \t \t Address = %p\n", i, cArray[i], &cArray[i]);
+
+    printf("\n\n");
+
+    return(0);
+}
