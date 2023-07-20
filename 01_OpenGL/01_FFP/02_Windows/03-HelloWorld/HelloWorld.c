649a320eeccbd9aaf35d1aae05ed30452537e543
VishwaKhude <97826054+VishwaKhude@users.noreply.github.com>
2023-07-20 20:04:57 +0530

    Windows

---
+++ 01_OpenGL/01_FFP/02_Windows/03-HelloWorld/HelloWorld.c
@@ -0,0 +1,97 @@
+// Windows Header Files
+#include <windows.h>          // same as <stdio.h> library function
+							  // includes around 3,50,000 API's 
+
+// Global Function Declarations (Global because called by OS, _far, _pascal)      
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); // (WndProc(Window Procedure) - Hungarian(Charles Simonee) Notation used) (CALLBACK FUNCTIONS), (HWND - Handle Window), (WPARAM - 16 Bit), (LPARAM - 32Bit)
+
+// Entry Point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow) //Winmain(main function which gives window(Hungarian Notation Used)), lpsz(Long Pointer To Zero-Terminated String ('\0')), hPrevInstance - for backward compatibility
+{
+	// Local Variable Declarations
+	WNDCLASSEX wndclass;  // (wndclass)window, class(type), type or class of window EX(WND(window) Extended)
+	HWND hwnd;            //handle window (Unsigned Int)
+	MSG msg;             // struct
+	TCHAR szAppName[] = TEXT(" VMKWindow "); // szAppName - Zero Terminated String To App-Name ('\0'), TEXT (macro)
+											 // TEXT(macro) - similar to 'printf'
+
+	//code
+	// WNDCEX initialization (Window Class Extended)
+	wndclass.cbSize = sizeof(WNDCLASSEX); // cbSize - Count Of Byte-size                                      
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;  // CS_HREDRAW - Class Style (Horizontal Re-Draw), CS_VREDRAW - (Vertical Re-Draw) // window should be drawn(modified) vertically and horizontally
+	wndclass.cbClsExtra = 0;  //c
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc; // lpfnWndProc - Long Pointer To Function - Window-Procedure
+	wndclass.hInstance = hInstance;  //Handle To Instance, (Camel Notation Used)
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); // hbrBackground - (handleTobrush) Background, BLACK_BRUSH = macro For Getting Brush Having Black Color
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION); // hIcon - handle To Icon, Icon is an - 'RESOURCE', ('NULL' Parameter because we have no icon and have to get one), ID - Identifier I - (Application) - Icon ,  
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);  // NULL Parameter because we don't have a cursor, hCursor(Handle To Cursor)  
+	wndclass.lpszClassName = szAppName;  // long-pointer to zero(NULL) terminated ('\0') Class Name
+	wndclass.lpszMenuName = NULL;      //Menu name - NULL (We Don't Have A Menu)
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION); 
+
+	// Register WNDCLASSEX
+	RegisterClassEx(&wndclass); //Registered Extended Class
+
+	// Create Window
+	hwnd = CreateWindow(szAppName, // string to NULL terminated app name('\0') 
+									 //Creates window in memory
+		TEXT("Vishwa Khude"), // TEXT(macro) UNICODE
+	WS_OVERLAPPEDWINDOW,  // (Window Style - OVERLAPPED(appearing above every application)
+		CW_USEDEFAULT,  // CW_USEDEFAULT - Create Window (Use Default Parameters) (X, Y, Width, Height) - 4 default parameters
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,    // Desktop Window(Defalut Handle), HWND_DESKTOP, (Parent Window Handle)      
+		NULL,    // NULL Parameter for 'Menu'
+		hInstance,    // Instance for Creating Handle For Window(OS)
+		NULL);   // used as an vessel (lpvoid(long parameter to void) - void*)
+
+	// Show The Window
+	ShowWindow(hwnd, iCmdShow);  // ShowWindow - Displaying Our Window, By Using (handle to window (hwnd)), 
+
+	// Paint/Re-Draw The Window
+	UpdateWindow(hwnd);    // Update(Display, Show) The Window using Black-Brush and display Black Window 
+
+	// Message-loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+
+	return((int)msg.wParam);
+}
+
+
+// CallBack Function
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) // UINT- Unsigned INTeger iMsg(integer Message) 
+{
+	// variable declarations
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rc;
+	TCHAR str[] = TEXT("Hello World!!!");
+
+	// code
+	switch (iMsg)  // Destroying The Window
+	{
+	case WM_PAINT :
+		GetClientRect(hwnd, &rc);
+		hdc = BeginPaint(hwnd, &ps);
+		SetBkColor(hdc, RGB(0, 0, 0));
+		SetTextColor(hdc, RGB(0, 255, 0));
+		DrawText(hdc, str, -1, &rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_DESTROY :  // GetMessage- False
+		PostQuitMessage(0);
+		break;
+	default :
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam)); // Default Window Procedure For returning the messages which are not be used in the program
+}
