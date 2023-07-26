d0156235df3d05d979d7f0e442d9612afa02f1fc
VishwaKhude <97826054+VishwaKhude@users.noreply.github.com>
2023-07-27 03:02:31 +0530

    Sync code with origin

--- 03_C_Assignments/14-Pointers/04-Structs/02-PointersWithinStructs/01-UsingStructVariable/PointerWithinStructs.c
+++ 03_C_Assignments/14-Pointers/04-Structs/02-PointersWithinStructs/01-UsingStructVariable/PointerWithinStructs.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct MyData
 {
@@ -16,49 +15,30 @@ struct MyData
 
 int main(void)
 {
-    //variabe declaration
-    struct MyData *pData = NULL;
+    //variabe declarations
+    struct MyData data;
 
     //code
-    printf("\n\n");
-    pData = (struct MyData *)malloc(sizeof(struct MyData));
-    if (pData == NULL)
-    {
-        printf("failed to allocate memory to 'struct MyData' !!! exitting now\n\n");
-        exit(0);
-    }
-    else
-        printf("Successfully allocated memory to 'struct MyData' !!!\n\n");
+    data.i = 9;
+    data.ptr_i = &data.i;
 
-   (*pData).i = 9;
-   (*pData).ptr_i = &(*pData).i;
+    data.f = 11.45f;
+    data.ptr_f = &data.f;
 
-   (*pData).f = 11.45f;
-   (*pData).ptr_f = &(*pData).f;
-
-   (*pData).d = 30.12995;
-   (*pData).ptr_d = &(*pData).d;
+    data.d = 30.121995;
+    data.ptr_d = &data.d;
+  
+   printf("\n\n");
+   printf("i = %d\n", *(data).ptr_i);
+   printf("Address of 'i' = %p\n", data.ptr_i);
 
    printf("\n\n");
-   printf("i = %d\n", *((*pData).ptr_i));
-   printf("address of 'i' = %p\n", (*pData).ptr_i);
+   printf("f = %f\n", *(data).ptr_f);
+   printf("address of 'f' = %p\n", data.ptr_f);
 
    printf("\n\n");
-   printf("f = %f\n", *((*pData).ptr_f));
-   printf("address of 'f' = %p\n", (*pData).ptr_f);
+   printf("d = %lf\n", *(data.ptr_d));
+   printf("address of 'd' = %p\n", data.ptr_d);
 
-   printf("\n\n");
-   printf("d = %lf\n", *((*pData).ptr_d));
-   printf("address of 'd' = %p\n", (*pData).ptr_d);
-
-    if(pData)
-    {
-        free(pData);
-        pData = NULL;
-        printf("Memory allocated to 'struct MyData' has been successfully freed !!!\n\n");
-    }
-    
     return(0);
-
-
 }
